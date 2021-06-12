3de886eca9fac5f7ec329c8cd32f42bb5bb0dd55
Marcus Ansley <56563445+M-Ansley@users.noreply.github.com>
2021-06-12 17:34:32 +0100

    Voiceovers on TriggerEnter

--- Assets/The Great Fleece/Game/Scripts/VoiceOverTrigger.cs
+++ Assets/The Great Fleece/Game/Scripts/VoiceOverTrigger.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class VoiceOverTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private AudioClip _voiceOverClip;
+
+    private GameObject _mainCameraObject = null;
+    private bool _played = false;
+
+
+    private void Start()
     {
-        
+        try
+        {
+            _mainCameraObject = Camera.main.gameObject;
+        }
+        catch { }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            if (!_played && _voiceOverClip != null)
+            {
+                if (_mainCameraObject != null)
+                {
+                    if (_mainCameraObject.TryGetComponent(out AudioSource audioSource))
+                    {
+                        audioSource.PlayOneShot(_voiceOverClip);
+                    }
+                }
+                else
+                {
+                    AudioSource.PlayClipAtPoint(_voiceOverClip, transform.position); // play it at the position of this object if not
+                }
+                _played = true;
+            }
+        }
     }
 }
