00a2129bbe53c35e2fffd5c952e1c20d02070ae3
Marcus Ansley <56563445+M-Ansley@users.noreply.github.com>
2021-06-13 18:36:26 +0100

    Get Keycard Cutscene Activation (1)

---
+++ Assets/The Great Fleece/Game/Scripts/GrabKeycardActivation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabKeycardActivation : MonoBehaviour
+{
+    [SerializeField] private GameObject _grabKeycardCutscene = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _grabKeycardCutscene.SetActive(true);
+        }
+    }
+}
