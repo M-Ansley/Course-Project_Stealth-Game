c26edd4638d85bf40ed19e7a690dc71565ee4f05
Marcus Ansley <56563445+M-Ansley@users.noreply.github.com>
2021-06-20 17:07:46 +0100

    Main Menu and Loading Scene

---
+++ Assets/The Great Fleece/Game/Scripts/MainMenu/LoadLevel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadLevel : MonoBehaviour
+{
+    public Image fillImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadLevelAsync());
+    }
+
+    IEnumerator LoadLevelAsync()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync("Main");
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            fillImage.fillAmount = progress;
+            Debug.Log(progress);
+            yield return null;
+        }
+
+        fillImage.fillAmount = 1;
+    }
+
+}
---
+++ Assets/The Great Fleece/Game/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+   public void StartGame()
+    {
+        Debug.Log("Should start game");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("LoadingScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
