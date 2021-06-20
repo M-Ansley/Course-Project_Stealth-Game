4cf8c462fcd21d856999ab62fc941cb909f98321
Marcus Ansley <56563445+M-Ansley@users.noreply.github.com>
2021-06-20 15:55:46 +0100

    UI Manager Setup

---
+++ Assets/The Great Fleece/Game/Scripts/UIManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _current;
+
+    public UIManager Current
+    {
+        get
+        {
+            if (_current == null)
+            {
+                Debug.LogError("UIManager is null");
+                return null;
+            }
+            else
+            {
+                return _current;
+            }
+        }
+    }
+
+
+    private void Awake()
+    {
+        _current = this;
+    }
+
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // restarts this scene
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+
+}
